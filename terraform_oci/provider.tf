ec80481f054ae80a769f85ec145f68f2b1138988
Thiago Melo <kmreisergs@gmail.com>
2023-02-02 11:53:34 +0000

    fix oci provider var

--- terraform_oci/provider.tf
+++ terraform_oci/provider.tf
@@ -3,11 +3,11 @@
 # https://github.com/reiserfs/k8s
 #
 provider "oci" {
-  user_ocid = ${var.user_ocid}
-  fingerprint = ${var.fingerprint}
-  tenancy_ocid = ${var.tenancy_ocid}
-  region  = ${var.cluster_zone}
-  private_key_path = ${var.private_key_path}
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  tenancy_ocid = var.tenancy_ocid
+  region  = var.cluster_zone
+  private_key_path = var.private_key_path
 }
 
 terraform {
